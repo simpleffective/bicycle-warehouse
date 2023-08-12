7c33e5f004609b89ddfbb63865f1a0e78c78fa21
Oren Pinkas <orenp95@gmail.com>
2023-08-12 19:11:59 +0300

    figuring out image upload

--- public/script.js
+++ public/script.js
@@ -2,26 +2,6 @@
 const TABLE_DATA_URL = '/get-table-data'
 const tableBody = document.querySelector('.bicycle-table').querySelector('tbody')
 let tableData = []
-{/* <tr>
-<!-- th scope=row for id for admins/devs? -->
-<td>
-  <div class="bicycle-visual-info">
-    <img class="bicycle-image" src="./resources/red-schwinn" alt="">
-    <span class="bicycle-visual-info-bottom">
-      <span class="bicycle-color"></span>
-      <span class="bicycle-text">schwinn</span>
-    </span>
-  </div>
-  <div class="status-container">
-  <span class="status-indicator"></span>
-  בטיפול
-</div>
-</td>
-<td>אורן פנקס</td>
-<td>orenp95&#8203@gmail.com</td>
-<td>15/06/2023</td>
-<td scope="row">11/07/2023</td>
-</td> */}
 
 function produceHTML(columnName, items) {
   if (columnName === 'visual') {
