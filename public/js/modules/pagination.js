a56911e62eedbf5a6a7caaf0f19698f68089bcd2
Oren Pinkas <orenp95@gmail.com>
2023-08-22 19:03:31 +0300

    Done with pagination for now.

--- public/js/modules/pagination.js
+++ public/js/modules/pagination.js
@@ -8,17 +8,13 @@ const ROWS_PER_PAGE = 12;
 let currentPage = 1;  // initial page
 const numbers = document.getElementById('pagination-numbers-container');
 // const paginationLength = numbers.querySelectorAll('button').length
-const paginationLength = 2;
+const paginationLength = 7;
 const prev = document.getElementById('prev')
 const next = document.getElementById('next')
-let prevButton = null;
-let currButton = null;
-let numRows = 0;
 let numPages = 0;
 
 function init(numRows) {
-
-  numRows = numRows;
+  
   numPages = Math.ceil(numRows / ROWS_PER_PAGE);
   
   prev.addEventListener('click', onPageRequest)
@@ -28,13 +24,14 @@ function init(numRows) {
 }
 
 function drawNumberButtons() {
+  
   numbers.innerHTML = '';
-  let firstButton = Math.min(Math.max(1, currentPage - 2), numRows - 4)
-  for (let i = firstButton ; i < firstButton + Math.min(numPages, paginationLength) ; i++) {
+  let start = Math.max(1, currentPage - Math.floor(paginationLength / 2));
+  let end = Math.min(numPages, start + paginationLength - 1);
+  for (let i = start ; i <= end ; i++) {
     let button = document.createElement('button');
     button.value = button.textContent = i;
     button.addEventListener('click', onPageRequest);
-    button.classList.add('pagination-button')
     if (i === currentPage)
       button.classList.add('current-page-button')
     numbers.appendChild(button);
@@ -42,7 +39,7 @@ function drawNumberButtons() {
 }
 
 function onPageRequest(evt) {
-
+  debugger
   let page = evt.target.value;
   if (page === '<')
     page = currentPage - 1;
